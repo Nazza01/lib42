fec25a4713dc3f9c522051e59ec4877df4a8c203
Nazza01 <nate_thomas_95@hotmail.com>
2022-06-23 17:53:18 +0930

    Revert "Adding function description for ft_error.c"
    This reverts commit bec020c024ea018e07b198bf0edbbca4747ed1c7.

--- sources/personal/ft_error.c
+++ sources/personal/ft_error.c
@@ -6,20 +6,18 @@
 /*   By: Nathanael <nervin@student.42adel.org.au    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 14:01:38 by Nathanael         #+#    #+#             */
-/*   Updated: 2022/05/20 13:57:33 by Nathanael        ###   ########.fr       */
+/*   Updated: 2022/04/07 14:46:41 by Nathanael        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib42.h"
 
-/**
- * @brief	Allows for a smooth exit to your program
- * @param	msg		The message to output
- * @param	code	The code to exit by
- * @return	Exits using system exit function
-**/
-void	ft_error(char *msg, int code)
+/*
+Prints an error using ft_printf then uses exit to return/exit the program
+Message format: <message>\n
+*/
+void	ft_error(char *message, int F_TYPE)
 {
-	ft_printf("%s", msg);
-	exit(code);
+	ft_printf("%s\n", message);
+	exit (F_TYPE);
 }
